152d2113184f996f2485fe4f4d8c5d5580512b0e
erf <easinerf@gmail.com>
2013-07-20 13:31:23 -0400

    First commit

---
+++ economy_graph.py
@@ -0,0 +1,193 @@
+import pygame
+from pygame.locals import *
+from pygame import Color
+
+
+class Label(object):
+	def __init__(self, group, font_size, text, text_color, rect_attribute, x, y, bground_color = "black"):
+		self.text = pygame.font.Font("freesansbold.ttf", font_size).render(text, True, Color(text_color), Color(bground_color))			
+		self.rect_pos_dict = {"topleft": self.text.get_rect(topleft = (x, y)),
+							 "topright": self.text.get_rect(topright = (x, y)),
+							 "bottomleft": self.text.get_rect(bottomleft = (x, y)),
+							 "bottomright": self.text.get_rect(bottomright = (x, y)),
+							 "midtop": self.text.get_rect(midtop = (x, y)),
+							 "midbottom": self.text.get_rect(midbottom = (x, y)),
+							 "center": self.text.get_rect(center = (x, y)),
+							 "midleft": self.text.get_rect(midleft = (x, y)),
+							 "midright": self.text.get_rect(midright = (x, y))}
+		self.rect = self.rect_pos_dict[rect_attribute]
+		group.append(self)		
+		
+class DataLine(object):
+	def __init__(self, name, point_list, color):
+		self.name = name
+		self.point_list = point_list
+		self.color = pygame.Color(color)
+		self.graphing = True
+		
+	def draw_line(self, surface, x_scale, y_scale, base):
+		scaled_point_list = [(point[0] * x_scale, base - int(point[1] * y_scale)) for point in self.point_list] 
+		pygame.draw.lines(surface, self.color, False, scaled_point_list)
+		
+def show_graph(economy):
+	pygame.init()
+	surface = pygame.display.set_mode((1080, 740))
+	pygame.display.set_caption("Price History")
+	FPS = 30
+	fpsClock = pygame.time.Clock()
+	SCREENWIDTH = 1080
+	HALFWIDTH = SCREENWIDTH / 2
+	SCREENHEIGHT = 740
+	HALFHEIGHT = SCREENHEIGHT / 2
+
+	text16 = pygame.font.Font("freesansbold.ttf", 16)
+	text20 = pygame.font.Font("freesansbold.ttf", 20)
+	
+	x_scale = 10
+	y_scale = 20
+	y_scale_mod = 1
+	base = 500
+	window = surface.get_rect()
+		
+	while x_scale * economy.cycle_count > window.width:
+		x_scale -= 1
+	data_lines = []
+	vert = base + 20
+	horiz = window.left + 50
+	for item in economy.price_history:
+		data_line = DataLine(item, economy.price_history[item][1:], economy.price_history[item][0]) 
+		data_line.graph_text = text16.render(data_line.name, True, data_line.color, Color("black"))
+		data_line.graph_text_rect = data_line.graph_text.get_rect(topleft = (horiz, vert))
+		vert += data_line.graph_text_rect.height + 10
+		if vert + data_line.graph_text_rect.height + 10 > window.bottom - 30:
+			vert = base + 20
+			horiz += 120
+		data_lines.append(data_line)
+	
+	blit_list = []
+	exit_label = Label(blit_list, 20, "Exit", "blue", "midbottom",
+						window.centerx, window.bottom - 20, "lightgray")
+	instruct1_label = Label(blit_list, 16, "Click on an item's name to toggle that line on the graph.",
+						"white", "topleft", exit_label.rect.right + 20, base + 50)
+	instruct2_label = Label(blit_list, 16, "Use the arrow keys to expand or compress the graph.",
+						"white", "topleft", instruct1_label.rect.left, instruct1_label.rect.bottom + 20)
+	all_off_label = Label(blit_list, 16, "Hide All", "blue", "bottomleft", window.left + 200, window.bottom - 10, "lightgray")
+	low_end_label = Label(blit_list, 16, "Low End", "blue", "bottomleft", window.left + 10, window.bottom - 10, "lightgray")
+	mid_range_label = Label(blit_list, 16, "Mid Range", "blue", "bottomleft", low_end_label.rect.right + 10, window.bottom - 10, "lightgray")
+	high_end_label = Label(blit_list, 16, "High End", "blue", "bottomleft", mid_range_label.rect.right + 10, window.bottom - 10, "lightgray")
+	
+	low_end = []
+	mid_range = []
+	high_end = []
+	for data_line in data_lines:
+		if data_line.name in ["Diamond", "Gold Ore", "Gold Ingot", "Iron Ore",
+				"Iron Ingot", "Leather", "Redstone Dust", "Coal", "Obsidian"]:
+			high_end.append(data_line)
+		elif data_line.name in ["Cobblestone", "Dirt", "Gravel", "Leaves",
+							"Cactus", "Pumpkin", "Sand", "Sapling",
+							"Wheat", "Wheat Seeds"]:
+			low_end.append(data_line)
+		else:
+			mid_range.append(data_line)
+			
+	expandingx = False
+	expandingy = False
+	compressingx = False
+	compressingy = False
+	
+	while True:
+		for event in pygame.event.get():
+			if event.type == MOUSEBUTTONDOWN:
+				x, y = event.pos
+				if exit_label.rect.collidepoint(x, y):
+					pygame.quit()
+					return False
+				elif all_off_label.rect.collidepoint(x, y):
+					for data_line in data_lines:
+						data_line.graphing = False
+				elif low_end_label.rect.collidepoint(x,y):
+					for data_line in high_end + mid_range:
+						data_line.graphing = False
+					for data_line in low_end:
+						data_line.graphing = True
+					y_scale = 300
+					y_scale_mod = 20
+				elif mid_range_label.rect.collidepoint(x, y):
+					for data_line in high_end + low_end:
+						data_line.graphing = False
+					for data_line in mid_range:
+						data_line.graphing = True
+					y_scale = 50
+					y_scale_mod = 5
+				elif high_end_label.rect.collidepoint(x, y):
+					for data_line in mid_range + low_end:
+						data_line.graphing = False
+					for data_line in high_end:
+						data_line.graphing = True
+					y_scale = 15
+					y_scale_mod = .5
+				for data_line in data_lines:
+					if data_line.graph_text_rect.collidepoint(x, y):
+						data_line.graphing = not data_line.graphing
+			elif event.type == KEYDOWN:
+				if event.key == K_RIGHT:
+					expandingx = True
+				elif event.key == K_UP:
+					expandingy = True
+				elif event.key == K_LEFT:
+					compressingx = True
+				elif event.key == K_DOWN:
+					compressingy = True
+				elif event.key == K_s:
+					pygame.image.save(surface, "screenshot.png")
+			elif event.type == KEYUP:
+				if event.key == K_RIGHT:
+					expandingx = False
+				elif event.key == K_UP:
+					expandingy = False
+				elif event.key == K_LEFT:
+					compressingx = False
+				elif event.key == K_DOWN:
+					compressingy = False
+		if expandingx:
+			x_scale += .2
+		elif expandingy:
+			y_scale += 1 * y_scale_mod
+		elif compressingx and x_scale > 0:
+			x_scale -= .2
+		elif compressingy and y_scale > 0:
+			y_scale -= 1 * y_scale_mod
+		
+		surface.fill(Color("black"))
+		for data_line in data_lines:
+			surface.blit(data_line.graph_text, data_line.graph_text_rect)
+			if data_line.graphing:
+				data_line.draw_line(surface, x_scale, y_scale, base)
+		for label in blit_list:
+			surface.blit(label.text, label.rect)
+		pygame.draw.line(surface, Color("white"), (window.left, base), (window.right, base))
+		pygame.display.update()
+		fpsClock.tick(FPS)
+
+def single_commodity_graph(economy, commodity):
+	pygame.init()
+	SCREENWIDTH = 1080
+	HALFWIDTH = SCREENWIDTH / 2
+	SCREENHEIGHT = 740
+	HALFHEIGHT = SCREENHEIGHT / 2
+	surface = pygame.Surface((SCREENWIDTH, SCREENHEIGHT))
+	data_line = DataLine(commodity, economy.price_history[commodity][1:], economy.price_history[commodity][0])
+	x_scale = 10
+	while economy.cycle_count * x_scale > SCREENWIDTH:
+		x_scale -= .5
+	max = 0
+	for point in economy.price_history[commodity][1:]:
+		if point[1] > max:
+			max = point[1]
+	y_scale = SCREENHEIGHT/max
+	surface.fill(Color("black"))
+	pygame.draw.line(surface, Color("white"), (economy.price_history[commodity][1][0] * x_scale, economy.price_history[commodity][1][1] * y_scale),
+						(SCREENWIDTH, economy.price_history[commodity][1][1] * y_scale) , 2)
+	data_line.draw_line(surface, x_scale, y_scale, SCREENHEIGHT)
+	pygame.image.save(surface, commodity + ".png")
+	
---
+++ server_manager.py
@@ -0,0 +1,438 @@
+import os
+import random
+import pickle
+import pygame
+import praw
+import pyimgur
+import economy_graph
+
+class Economy(object):
+	def __init__(self):
+		self.base_prices = {"Cactus": .04,
+						"Coal": 2.00,
+						"Cobblestone": .05,
+						"Cooked Fish": .10,
+						"Diamond": 50.00,
+						"Dirt": .05,
+						"Flower": .25,
+						"Gold Ore": 15.00,
+						"Gold Ingot": 16.5,
+						"Gravel": .05,
+						"Iron Ore": 3.00,
+						"Iron Ingot": 3.50,
+						"Leaves": .05,
+						"Leather": 2.50,
+						"Melon Slice": .07,
+						"Mushroom": .25,
+						"Obsidian": 7.50,
+						"Pumpkin": .03,
+						"Redstone Dust": 2.00,
+						"Sand": .05,
+						"Sandstone": .20,
+						"Sapling": .025,
+						"Wheat": .05,
+						"Wheat Seeds": .08,}
+		self.prices = {"Cactus": .04,
+						"Coal": 2.00,
+						"Cobblestone": .05,
+						"Cooked Fish": .10,
+						"Diamond": 50.00,
+						"Dirt": .05,
+						"Flower": .25,
+						"Gold Ore": 15.00,
+						"Gold Ingot": 16.5,
+						"Gravel": .05,
+						"Iron Ore": 3.00,
+						"Iron Ingot": 3.50,
+						"Leaves": .05,
+						"Leather": 2.50,
+						"Melon Slice": .07,
+						"Mushroom": .25,
+						"Obsidian": 7.50,
+						"Pumpkin": .03,
+						"Redstone Dust": 2.00,
+						"Sand": .05,
+						"Sandstone": .20,
+						"Sapling": .025,
+						"Wheat": .05,
+						"Wheat Seeds": .08}
+		
+		# index [0] = line color,
+		# appended elements will be tuples of floats
+		self.price_history = {"Cactus": ["limegreen"],		
+						"Coal": ["gray16"],
+						"Cobblestone": ["lightgray"],
+						"Cooked Fish": ["cyan"],
+						"Diamond": ["cadetblue1"],
+						"Dirt": ["chocolate4"],
+						"Flower": ["yellow"],
+						"Gold Ore": ["gold"],
+						"Gold Ingot": ["gold2"],
+						"Gravel": ["gray29"],
+						"Iron Ore": ["gray60"],
+						"Iron Ingot": ["gray40"],
+						"Leaves": ["darkolivegreen"],
+						"Leather": ["chocolate1"],
+						"Melon Slice": ["hotpink"],
+						"Mushroom": ["red2"],
+						"Obsidian": ["purple4"],
+						"Pumpkin": ["orangered"],
+						"Redstone Dust": ["red"],
+						"Sand": ["navajowhite"],
+						"Sandstone": ["peachpuff3"],
+						"Sapling": ["chartreuse3"],
+						"Wheat": ["tan2"],
+						"Wheat Seeds": ["tan4"]}
+						
+		self.volatilities = ["Calm", "Active", "Frantic"]
+		self.volatility_mods = {"Calm": 10,
+								"Active": 20,
+								"Frantic": 40}
+		self.next_volatility = {"Calm": ["Active", "Calm", "Calm"],
+								"Active": ["Calm", "Active", "Active", "Frantic"],
+								"Frantic": ["Calm", "Active", "Active", "Frantic"]}
+		self.volatility = "Calm"
+		
+		self.trends = ["Horrendous", "Weak", "Stable", "Strong", "Phenomenal"]
+		self.trend_mods = {"Phenomenal": 15,
+							"Strong": 10,
+							"Stable": 1,
+							"Weak": -5,
+							"Horrendous": -10}
+		self.next_trend = {"Phenomenal": ["Phenomenal", "Strong", "Strong", "Strong"],
+							"Strong": ["Phenomenal", "Strong", "Strong", "Stable"],
+							"Stable": ["Weak", "Stable", "Stable", "Strong"],
+							"Weak": ["Stable", "Weak", "Weak", "Horrendous"],
+							"Horrendous": ["Stable", "Weak", "Weak", "Horrendous"]}
+		self.trend = "Stable"
+		self.cycle_count = 0
+		self.checked_ids = []
+		self.citizens = {"RebuiltOak624": {"QCE Account": 100,
+											"Username": "iminurnamez",
+											"District": "Norhigashi",
+											"Title": "Citizen",
+											"Trade Hold": False,
+											"Gladiator Wins": 0,
+											"Gladiator Losses": 0,
+											"Inventory": {"Cactus": 0,		
+														"Coal": 0,
+														"Cobblestone": 0,
+														"Cooked Fish": 0,
+														"Diamond": 0,
+														"Dirt": 0,
+														"Flower": 0,
+														"Gold Ore": 0,
+														"Gold Ingot": 0,
+														"Gravel": 0,
+														"Iron Ore": 0,
+														"Iron Ingot": 0,
+														"Leaves": 0,
+														"Leather": 0,
+														"Melon Slice": 0,
+														"Mushroom": 0,
+														"Obsidian": 0,
+														"Pumpkin": 0,
+														"Redstone Dust": 0,
+														"Sand": 0,
+														"Sandstone": 0,
+														"Sapling": 0,
+														"Wheat": 0,
+														"Wheat Seeds": 0}}}
+														
+	def update(self):
+		self.previous_prices = {}
+		for commodity in self.prices:
+			self.previous_prices[commodity] = self.prices[commodity]
+		self.trend = random.choice(self.next_trend[self.trend])
+		self.volatility = random.choice(self.next_volatility[self.volatility])
+		
+		for item in self.prices:
+			if random.randint(1, 100) < self.volatility_mods[self.volatility]:
+				x = 50
+				if self.prices[item] > self.base_prices[item] * 3:
+					x = 10
+				elif self.prices[item] > self.base_prices[item] * 2:
+					x = 15
+				elif self.prices[item] > self.base_prices[item] * 1.5:
+					x = 25
+				elif self.prices[item] > self.base_prices[item] * 1.25:
+					x = 40
+				elif self.prices[item] < self.base_prices[item] * .8:
+					x = 60
+				elif self.prices[item] < self.base_prices[item] * .6:
+					x = 75
+				elif self.prices[item] < self.base_prices[item] * .4:
+					x = 85
+				elif self.prices[item] < self.base_prices[item] * .3:
+					x = 90
+				x += self.trend_mods[self.trend]
+				if random.randint(1, 100) < x:
+					self.prices[item] += self.prices[item] * random.uniform(0.0, 0.2)
+				else:
+					self.prices[item] -= self.prices[item] * random.uniform(0.0, 0.2)
+				
+		
+		for item in self.prices:
+			self.price_history[item].append((self.cycle_count, self.prices[item]))
+		self.cycle_count += 1
+
+def post_prices(economy):
+	sub_name = "Quadraria"
+	bot_name = "QuadrarianEconomy"
+	bot_password = ""
+	
+	bot = praw.Reddit(user_agent = bot_name)
+	bot.login(bot_name, bot_password)
+	text = "Commodity|Price|Change\n:---|---:|---:"
+	for item in economy.prices:
+		text += "\n{0}|{1:.2f}|{2:+.4f}".format(item, economy.prices[item], economy.prices[item] - economy.previous_prices[item])
+	text += "\n\nDisplayed prices are rounded to nearest hundredth"
+	bot.submit(sub_name, "QCE Price Update", text)
+		
+def add_citizen(economy):
+	gamertag = raw_input("Enter gamertag:")
+	username = raw_input("Enter username:")
+	district = raw_input("Enter district:")
+	title = raw_input("Enter title:")
+	economy.citizens[gamertag] = {"QCE Account": 0,
+								"Username": username,
+								"District": district,
+								"Title": title,
+								"Trade Hold": False,
+								"Gladiator Wins": 0,
+								"Gladiator Losses": 0,
+								"Inventory": {"Cactus": 0,		
+											"Coal": 0,
+											"Cobblestone": 0,
+											"Cooked Fish": 0,
+											"Diamond": 0,
+											"Dirt": 0,
+											"Flower": 0,
+											"Gold Ore": 0,
+											"Gold Ingot": 0,
+											"Gravel": 0,
+											"Iron Ore": 0,
+											"Iron Ingot": 0,
+											"Leaves": 0,
+											"Leather": 0,
+											"Melon Slice": 0,
+											"Mushroom": 0,
+											"Obsidian": 0,
+											"Pumpkin": 0,
+											"Redstone Dust": 0,
+											"Sand": 0,
+											"Sandstone": 0,
+											"Sapling": 0,
+											"Wheat": 0,
+											"Wheat Seeds": 0}}
+	print "Citizen added"
+			
+def remove_citizen(economy):
+	gamertag = raw_input("Enter gamertag:")
+	if gamertag in economy.citizens:
+		double_check = raw_input("Are you sure you want to delete {0}?".format(gamertag))
+		if double_check in ("y", "Y", "yes", "Yes"):
+			del economy.citizens[gamertag]
+			print "{0} has been removed".format(gamertag)
+			
+def change_citizenship(economy):
+	gamertag = raw_input("Enter gamertag:")
+	if gamertag in economy.citizens:
+		new_district = raw_input("Enter new district:")
+		double_check = raw_input("Are you sure you want to change {0}'s district to {1}?".format(gamertag, new_district))
+		if double_check in ("y", "Y", "yes", "Yes"):
+			economy.citizens[gamertag]["District"] = new_district
+			print "{0} is now a citizen of {1}".format(gamertag, new_district)
+			
+def economy_handler(economy): 
+	print economy.volatility
+	for item in economy.prices:
+		print "{0}:\t\t{1:.2f}\t\t{2}".format(item, economy.prices[item], economy.prices[item] - economy.base_prices[item])
+	post_prices(economy)
+	economy.update()
+	print
+	print economy.trend
+	
+def trade_handler(economy, author, gamertag, transaction_type, commodity, quantity):
+	if economy.citizens[gamertag]["Username"] != str(author):
+		return "Username/gamertag authorization failed"
+	if economy.citizens[gamertag]["Trade Hold"]:
+		return "Your trading rights are currently suspended"
+	if transaction_type == "BUY":
+		if economy.prices[commodity] * quantity <= economy.citizens[gamertag]["QCE Account"]:
+			economy.citizens[gamertag]["QCE Account"] -= economy.prices[commodity] * quantity
+			economy.citizens[gamertag]["Inventory"][commodity] += quantity
+			return "Transaction Completed: {0} {1} has been credited to your QCE account".format(quantity, commodity)
+		else:
+			return "Insufficient Funds"
+	elif transaction_type == "SELL":
+		if economy.citizens[gamertag]["Inventory"][commodity] >= quantity:
+			economy.citizens[gamertag]["QCE Account"] += economy.prices[commodity] * quantity
+			economy.citizens[gamertag]["Inventory"][commodity] -= quantity
+			return "Transaction Completed: ${0:.2f} has been credited to your QCE account".format(economy.prices[commodity] * quantity)
+		else:
+			return "Insufficient Commodity Amount"
+
+def exchange_handler(economy):
+	gamertag = raw_input("\t\tEnter gamertag:")
+		
+	while True:
+		option = raw_input("\t\tCurrent Account: {0}\n\
+		Choose transaction type:\n\
+		1) Withdrawal\n\
+		2) Deopsit\n\
+		3) Change gamertag\n\
+		4) Display Account\n\
+		9) Quit\n\t\t>".format(gamertag))
+		
+		if option == "1":
+			commodity = raw_input("\t\tEnter \"Cash\" or Commodity name to withdraw:\n\t\t>")
+			qty = int(raw_input("\t\tEnter withdrawal amount:"))
+			if commodity == "Cash":
+				if economy.citizens[gamertag]["QCE Account"] >= qty:
+					economy.citizens[gamertag]["QCE Account"] -= qty
+					print "\n\t\t${0} withdrawn from {1}'s account\n".format(qty, gamertag)
+				else:
+					print "Insufficient funds"
+			elif commodity in economy.citizens[gamertag]["Inventory"].keys():
+				if economy.citizens[gamertag]["Inventory"][commodity] >= qty:
+					economy.citizens[gamertag]["Inventory"][commodity] -= qty
+					print "\n\t\t{0} {1} withdrawn from {2}'s account\n".format(qty, commodity, gamertag)
+				else:
+					print "Insufficient commodity amount"
+		
+		elif option == "2":
+			commodity = raw_input("\t\tEnter \"Cash\" or Commodity name to deposit\n\t\t>")
+			qty = int(raw_input("\t\tEnter deposit amount:"))
+			if commodity == "Cash":
+				economy.citizens[gamertag]["QCE Account"] += qty
+				print "\n\t\t${0} deposited to {1}'s account\n".format(qty, gamertag)
+			else:
+				economy.citizens[gamertag]["Inventory"][commodity] += qty
+				print "\n\t\t{0} {1} deposited to {2}'s account\n".format(qty, commodity, gamertag)
+			
+		elif option == "3":
+			gamertag = raw_input("\t\tEnter gamertag:")
+			
+		elif option == "4":
+			print "\n\n\n\t\tBalance: ${0:.2f}\n".format(economy.citizens[gamertag]["QCE Account"])
+			for commodity in economy.citizens[gamertag]["Inventory"]:
+				print "\t\t{0:20}\t{1:>8}".format(commodity, economy.citizens[gamertag]["Inventory"][commodity])
+				
+		elif option == "9":
+			return
+	
+def run_bot(economy):
+	sub_name = "Quadraria"
+	bot_name = "QuadrarianEconomy"
+	bot_password = ""
+	imgur_client_id = ""
+	
+	bot = praw.Reddit(user_agent = bot_name)
+	bot.login(bot_name, bot_password)
+	submissions = bot.get_subreddit(sub_name).get_new()
+	for submission in submissions:
+		if submission.id not in economy.checked_ids and "#" in submission.title:
+			title = submission.title
+			author = str(submission.author)
+			gamertag = str(submission.author_flair_text)
+			reply = ""
+			for commodity in economy.prices:
+				if commodity in title and "#GRAPH" not in title:
+					if commodity == "Wheat" and "Wheat Seeds" in title:
+						pass
+					else:
+						try:
+							quantity = int(submission.selftext)
+						except ValueError:
+							economy.checked_ids.append(submission.id)
+							reply += "Post text must be a number"
+							break
+						if "#BUY" in title:
+							reply += trade_handler(economy, author, gamertag, "BUY", commodity, quantity)
+						elif "SELL" in title:
+							reply += trade_handler(economy, author, gamertag, "SELL", commodity, quantity)
+						break
+			if "#ACCT INQ" in submission.title:
+				if economy.citizens[gamertag]["Username"] != author:
+					reply += "Username/gamertag authorization failed"
+				else:
+					reply += "Balance: {0:.2f}".format(economy.citizens[gamertag]["QCE Account"])
+					reply += "\n\nCommodity|Inventory"
+					reply += "\n:---|---:"
+					for commodity in economy.prices:
+						reply += "\n{0}|{1}".format(commodity, economy.citizens[gamertag]["Inventory"][commodity])
+			if "#GRAPH" in submission.title:
+				for commodity in economy.prices:
+					if commodity in title:
+						if commodity == "Wheat" and "Wheat Seeds" in submission.title:
+							pass
+						else:
+							economy_graph.single_commodity_graph(economy, commodity)
+							path = os.path.join(commodity + ".png")
+							imgur = pyimgur.Imgur(imgur_client_id)
+							uploaded_image = imgur.upload_image(path, title=commodity)
+							print(uploaded_image.link)
+							reply += "[Graph]({0})".format(uploaded_image.link)
+							os.remove(os.path.join(commodity + ".png"))
+			economy.checked_ids.append(submission.id)			
+			if len(reply) > 1:
+				submission.add_comment(reply)
+
+def clean_up_sub(economy):
+	sub_name = "Quadraria"
+	mod_name = "iminurnamez"
+	mod_password = ""
+	
+	bot = praw.Reddit(user_agent = sub_name + "Mod")
+	bot.login(mod_name, mod_password)
+	submissions = bot.get_subreddit(sub_name).get_new()
+	for submission in submissions:
+		if submission.id in economy.checked_ids:
+			submission.remove()
+			
+def calc_building_cost(economy):
+	width = int(raw_input("Enter building width:"))
+	length = int(raw_input("Enter building length:"))
+	height = int(raw_input("Enter building height:"))
+	material = raw_input("Enter building material:")
+	num_blocks  = (2 * length + 2 * width) * height
+	materials_cost = num_blocks * economy.prices[material]
+	labor_cost = num_blocks * .001
+	print "Pay upon completion: ${0}".format(materials_cost + labor_cost)
+	
+def main():
+	try:
+		economy = pickle.load(open("economy_save.pickle", "rb"))
+	except IOError:
+		print "Couldn't load economy_save.pickle"
+		economy = Economy()
+	
+	option = int(raw_input("\n1) Update Economy\n2) Draw Graph\n3) Single Commodity Graph\n4) \
+Run Bot\n5) Manage Citizens\n6) Manage Accounts\n7) Building Cost Calculator\n8) Clean Up Sub\n\nEnter option number:"))
+	if option == 1:
+		economy_handler(economy)
+	elif option == 2:
+		economy_graph.show_graph(economy)
+	elif option == 3:
+		economy_graph.single_commodity_graph(economy)
+	elif option == 4:
+		run_bot(economy)
+	elif option == 5:
+		citizen_option = raw_input("\n\t\tChoose an option\n\t\t1) Add citizen\n\t\t2) Remove citizen\n\t\t")
+		if citizen_option == "1":
+			add_citizen(economy)
+		elif citizen_option == "2":
+			remove_citizen(economy)
+	elif option == 6:
+		exchange_handler(economy)
+	elif option == 7:
+		calc_building_cost(economy)
+	elif option == 8:
+		if raw_input("Are you sure? This will remove posts from the sub.>") in ("y", "Y", "yes", "Yes"):
+			clean_up_sub(economy)
+		
+	pickle.dump(economy, open("economy_save.pickle", "wb"))
+
+if __name__ == "__main__":		
+	main()
